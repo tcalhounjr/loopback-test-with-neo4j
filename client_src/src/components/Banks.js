0e27e94a1758dddd748d0950fd333d3796220ffa
tcalhounjr <thomas@nafasi.co>
2018-08-17 23:13:33 -0400

    Another Banks.js update

--- client_src/src/components/Banks.js
+++ client_src/src/components/Banks.js
@@ -16,7 +16,7 @@ class Banks extends Component {
     }
 
     getBanks() {
-        axios.get('https://hobby-kibaaikfepomgbkedeklinbl.dbs.graphenedb.com/api/Banks')
+        axios.get('https://salty-plains-43075.herokuapp.com/api/Banks')
         .then(
             response => {
                 this.setState({banks: response.data}, () => {
